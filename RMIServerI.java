9f84e3a2d49dd26349fd1047ebaa0eab72d9bfd2
BenjaminIrwin <bti19@ic.ac.uk>
2020-02-11 11:48:47 +0000

    Upload New File

---
+++ RMIServerI.java
@@ -0,0 +1,15 @@
+/*
+ * Created on 01-Mar-2016
+ */
+package rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import common.*;
+
+// Clients program to remote interfaces, not to the implementation classes of those interfaces.
+
+public interface RMIServerI extends Remote {
+	public void receiveMessage(MessageInfo msg) throws RemoteException;
+}
